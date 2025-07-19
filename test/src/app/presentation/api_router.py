a1508b86fd7ef8c69bfdb36b93ff9fb17e8b31fc
Clonexy <conexy700@gmail.com>
2025-07-19 19:41:31 +0700

    Revert "Initial commit"
    This reverts commit 6f14378055948eb543c240dd071da18e9355e400.

--- test/src/app/presentation/api_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.app.presentation.v1.routers import shift_tasks
-from src.app.presentation.v1.routers import product
-
-api_router = APIRouter()
-api_router.include_router(shift_tasks.router, prefix="/api/v1/shift-tasks", tags=["Shift Tasks"])
-api_router.include_router(product.router, prefix="/api/v1/products", tags=["Products"])
--- test/src/app/presentation/deps.py
+++
@@ -1,7 +0,0 @@
-from Demos.security.lsaregevent import ret_code
-from fastapi import Depends
-from sqlalchemy.orm import Session
-from src.app.infrastructure.db.session import get_bd as _get_db
-
-def get_db() -> Session:
-    return Depends(_get_db)
